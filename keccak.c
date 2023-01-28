8652cf3ed0d8fcb8ea829b583b5763a2df5cd08e
ronan randles <ronanrandles@hotmail.com>
2023-01-28 15:17:20 +0000

    Absorbtion working, keccak function 95% functional. Acceptable stage to start cuda work

--- keccak.c
+++ keccak.c
@@ -28,15 +28,21 @@
   * @param  outputByteLen   The number of output bytes desired.
   * @pre    One must have r+c=1600 and the rate a multiple of 8 bits in this implementation.
   */
-void Keccak(unsigned int rate, unsigned int capacity, const unsigned char *input, unsigned long long int inputByteLen, unsigned char delimitedSuffix, unsigned char *output, unsigned long long int outputByteLen);
+void Keccak(unsigned int rate, unsigned int capacity, unsigned char *input, unsigned long long int inputByteLen, unsigned char delimitedSuffix, unsigned char *output, unsigned long long int outputByteLen);
 
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 typedef uint64_t tKeccakLane;
 
 #define ROL64(a, offset) ((((uint64_t)a) << offset) ^ (((uint64_t)a) >> (64-offset)))
 #define i(x, y) ((x)+5*(y))
 #define Keccak_Rounds 24
+#define SHA3_CONST(x) x##L
+enum last_permutation {Theta, Rho_PI, Chi, Iota};
+char * perm_strings [4] = {"Theta", "Rho_PI", "Chi", "Iota"};
+enum last_permutation last_perm;
 
 // Works for Little Endian system, more needed to support Big Endian
 #define readLane(x, y)          (((tKeccakLane*)state)[i(x, y)])
@@ -51,24 +57,41 @@ uint64_t sample_input[25] =     {0xa3a3a3a3a3a3a3a3, 0xa3a3a3a3a3a3a3a3, 0xa3a3a
                                  0xa3a3a3a3a3a3a3a3, 0xa3a3a3a3a3a3a3a3, 0x0, 0x0, 0x0,
                                  0x0, 0x0, 0x0, 0x0, 0x0};
 
-int LFSR86540(uint8_t *LFSR)
-{
-    int result = ((*LFSR) & 0x01) != 0;
-    if (((*LFSR) & 0x80) != 0)
-        /* Primitive polynomial over GF(2): x^8+x^6+x^5+x^4+1 */
-        (*LFSR) = ((*LFSR) << 1) ^ 0x71;
-    else
-        (*LFSR) <<= 1;
-    return result;
-}
+unsigned char *sample_input_2 = "0xa3a3a3a3a3a3a3a3,0xa3a3a3a3a3a3a3a3,0xa3a3a3a3a3a3a3a3,0xa3a3a3a3a3a3a3a3,0xa3a3a3a3a3a3a3a3,0xa3a3a3a3a3a3a3a3,0xa3a3a3a3a3a3a3a3,0xa3a3a3a3a3a3a3a3,0xa3a3a3a3a3a3a3a3,0xa3a3a3a3a3a3a3a3,0xa3a3a3a3a3a3a3a3,0xa3a3a3a3a3a3a3a3,0xa3a3a3a3a3a3a3a3,0xa3a3a3a3a3a3a3a3,0xa3a3a3a3a3a3a3a3,0xa3a3a3a3a3a3a3a3,0xa3a3a3a3a3a3a3a3,0x0000000000000000,0x0,0x0000000000000000,0x0000000000000000,0x0000000000000000,0x0000000000000000,0x0000000000000000,0x0000000000000000";
 
-static void print_state(uint8_t state[25]){
+// int LFSR86540(uint8_t *LFSR)
+// {
+//     int result = ((*LFSR) & 0x01) != 0;
+//     if (((*LFSR) & 0x80) != 0)
+//         /* Primitive polynomial over GF(2): x^8+x^6+x^5+x^4+1 */
+//         (*LFSR) = ((*LFSR) << 1) ^ 0x71;
+//     else
+//         (*LFSR) <<= 1;
+//     return result;
+// }
+
+static const uint64_t keccakf_rndc[24] = {
+    SHA3_CONST(0x0000000000000001UL), SHA3_CONST(0x0000000000008082UL),
+    SHA3_CONST(0x800000000000808aUL), SHA3_CONST(0x8000000080008000UL),
+    SHA3_CONST(0x000000000000808bUL), SHA3_CONST(0x0000000080000001UL),
+    SHA3_CONST(0x8000000080008081UL), SHA3_CONST(0x8000000000008009UL),
+    SHA3_CONST(0x000000000000008aUL), SHA3_CONST(0x0000000000000088UL),
+    SHA3_CONST(0x0000000080008009UL), SHA3_CONST(0x000000008000000aUL),
+    SHA3_CONST(0x000000008000808bUL), SHA3_CONST(0x800000000000008bUL),
+    SHA3_CONST(0x8000000000008089UL), SHA3_CONST(0x8000000000008003UL),
+    SHA3_CONST(0x8000000000008002UL), SHA3_CONST(0x8000000000000080UL),
+    SHA3_CONST(0x000000000000800aUL), SHA3_CONST(0x800000008000000aUL),
+    SHA3_CONST(0x8000000080008081UL), SHA3_CONST(0x8000000000008080UL),
+    SHA3_CONST(0x0000000080000001UL), SHA3_CONST(0x8000000080008008UL)
+};
+
+static void print_state(uint64_t state[25], int round){
     int x, y;
-
+    printf("%s round %i\n", perm_strings[last_perm], round);
     //print in format of sha3 example pdf
     for (int p = 0; p < 5; p++) {
         for (int l = 0; l < 5; l++) {
-            printf("%.16lx ", readLane(l,p));
+            printf("[%i,%i]: %.16lx ", l, p, readLane(l,p));
             if (((p * 5 + l) % 2) == 1)
                 printf("\n");
         }
@@ -76,13 +99,19 @@ static void print_state(uint8_t state[25]){
     printf("\n");
 }
 
+// void ReverseLane(void *state, int round){
+//     printf("Lane [%i, %i]: %.16lx reversed: %.16lx \n", round % 5, (int)(round/5), readLane(round % 5, (int)(round/5)), bswap_64(readLane(round % 5, (int)(round/5))));
+//     writeLane((int)(round/5),round % 5,bswap_64(readLane(round % 5, (int)(round/5))));
+// }
+
 void KeccakF1600(void *state)
 {
     unsigned int round, x, y, j, t;
-    uint8_t LFSRstate = 0x01;
-
-    for (round = 0; round < 1; round++)
+    // uint8_t LFSRstate = 0x01;
+    print_state(state,0);
+    for (round = 0; round < 25; round++)
     {
+        //print_state(state, round);
         //Theta step
         {
             tKeccakLane C[5], D;
@@ -93,10 +122,11 @@ void KeccakF1600(void *state)
                 D = C[(x + 4) % 5] ^ ROL64(C[(x+1) % 5], 1);
                 for (y = 0; y < 5; y++)
                     XORLane(x, y, D);
-            }    
+            } 
+            last_perm = Theta;
+            //print_state(state, round);
         }
         //Rho and Pi step
-        //TODO (non-essential): Split Rho and Pi for sake of separation from reference material
         {
             tKeccakLane current, tmp;
 
@@ -114,6 +144,8 @@ void KeccakF1600(void *state)
                 writeLane(x, y, ROL64(current, r));
                 current = tmp;
             }
+            last_perm = Rho_PI;
+            //print_state(state, round);
         }
         //Chi step
         {
@@ -124,77 +156,124 @@ void KeccakF1600(void *state)
                 for(x = 0; x < 5; x++)
                     writeLane(x, y, temp[x] ^ ((~ temp[(x + 1) % 5]) & temp[(x + 2) % 5]));
             }
+            last_perm = Chi;
+            //print_state(state, round);
         }
+        
         //Iota step
         {
-            for (j = 0; j < 7; j++) {
-                unsigned int bitPosition = (1 << j) - 1; /* (2^j)-1 */
-                if (LFSR86540(&LFSRstate))
-                    XORLane(0, 0, (tKeccakLane)1 << bitPosition);
-            }
+            // for (j = 0; j < 7; j++) {
+            //     unsigned int bitPosition = (1 << j) - 1; /* (2^j)-1 */   
+                   
+            //     if (LFSR86540(&LFSRstate)) {
+            //         XORLane(0, 0, (tKeccakLane)1 << bitPosition);
+            //     }
+            // }
+            XORLane(0,0,keccakf_rndc[round]);
+            last_perm = Iota;
+            print_state(state, round);
         }
+        
     }
 
-    print_state(state);
+    
 }
 
 #include <string.h>
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAXSTRLEN sizeof("0xa3a3a3a3a3a3a3a3")
 
-void Keccak(unsigned int rate, unsigned int capacity, const unsigned char *input, unsigned long long int inputByteLen, unsigned char delimitedSuffix, unsigned char *output, unsigned long long int outputByteLen)
+void Keccak(unsigned int rate, unsigned int capacity, unsigned char *input, unsigned long long int inputByteLen, unsigned char delimitedSuffix, unsigned char *output, unsigned long long int outputByteLen)
 {
-    uint8_t state[200];
+
+    uint64_t state[25];
     unsigned int rateInBytes = rate/8;
     unsigned int blockSize = 0;
     unsigned int i;
+    unsigned char *ptr;
+    char *new_ptr;
+    char *remaining;
+    unsigned char *prev_ptr;
+    int individual_length;
+    char *temp = malloc(MAXSTRLEN);
+
+    memset(state, 0, sizeof(state));
+    // //Absorb input blocks from sting using parsing
+    ptr = strchr(input,',');
+    individual_length = (unsigned char *)ptr - input;
+    memcpy(temp, input, individual_length);
+    for (int i = 0; i < sizeof(state)/8; i++){
+        state[i] = strtoul(temp,&remaining,16);
+
+        if (!ptr)
+            break;
+        memset(temp, 0, MAXSTRLEN);
+        memcpy(temp, ++ptr, individual_length);
+
+        //Save pointer for future length calculations
+        prev_ptr = ptr;
+        
+        //Set new ponter, Incrementation operator skips delimiting comma
+        ptr = strchr(ptr,',');
+
+        //Allow "0x0" to be used instead of "0x000...0"
+        individual_length = ptr - prev_ptr;
+    }
 
     //Ensure rate and capacity sum to 1600 for keccakF1600
     if (((rate + capacity) != 1600) || ((rate % 8) != 0))
     return;
 
-    memset(state, 0, sizeof(state));
-
-    //Absorb input blocks
-    while(inputByteLen > 0) {
-        blockSize = MIN(inputByteLen, rateInBytes);
-        for (i = 0; i < blockSize; i++)
-            state[i] ^= input[i];
-        input += blockSize;
-        inputByteLen -= blockSize;
-
-        if (blockSize == rateInBytes) {
-            KeccakF1600(state);
-            blockSize = 0;
-        }
-    }
-
-    /* === Do the padding and switch to the squeezing phase === */
-    /* Absorb the last few bits and add the first bit of padding (which coincides with the delimiter in delimitedSuffix) */
-    state[blockSize] ^= delimitedSuffix;
-    /* If the first bit of padding is at position rate-1, we need a whole new block for the second bit of padding */
-    if (((delimitedSuffix & 0x80) != 0) && (blockSize == (rateInBytes-1)))
-        KeccakF1600(state);
-    /* Add the second bit of padding */
-    state[rateInBytes-1] ^= 0x80;
-    /* Switch to the squeezing phase */
     KeccakF1600(state);
 
-    /* === Squeeze out all the output blocks === */
-    while(outputByteLen > 0) {
-        blockSize = MIN(outputByteLen, rateInBytes);
-        memcpy(output, state, blockSize);
-        output += blockSize;
-        outputByteLen -= blockSize;
+    // while(inputByteLen > 0) {
+    //     blockSize = MIN(inputByteLen, rateInBytes);
+       
+    //     // for (i = 0; i < blockSize; i++){
+    //     //     state[i] ^= input[i];
+    //     //     printf("Input: %x\n", input[i]);
+    //     // }
+    //     char *input_copy = strtok_r(input,",", &token);
+    //     printf("Token: %s\n", token);
+    //     state[count] = atoi(token);
+    //     //print_state(&state, count++);
+    //     input += blockSize;
+    //     inputByteLen -= blockSize;
 
-        if (outputByteLen > 0)
-            KeccakF1600(state);
-    }
+    //     if (inputByteLen == blockSize) {
+    //         //print_state(state, 999);
+    //         //KeccakF1600(state);
+    //         blockSize = 0;
+    //     }
+    // }
+    
+    // printf("SQEEZING\n");
+    // /* === Do the padding and switch to the squeezing phase === */
+    // /* Absorb the last few bits and add the first bit of padding (which coincides with the delimiter in delimitedSuffix) */
+    // state[blockSize] ^= delimitedSuffix;
+    // /* If the first bit of padding is at position rate-1, we need a whole new block for the second bit of padding */
+    // if (((delimitedSuffix & 0x80) != 0) && (blockSize == (rateInBytes-1)))
+    //     KeccakF1600(state);
+    // /* Add the second bit of padding */
+    // state[rateInBytes-1] ^= 0x80;
+    // /* Switch to the squeezing phase */
+    // KeccakF1600(state);
+
+    // /* === Squeeze out all the output blocks === */
+    // while(outputByteLen > 0) {
+    //     blockSize = MIN(outputByteLen, rateInBytes);
+    //     memcpy(output, state, blockSize);
+    //     output += blockSize;
+    //     outputByteLen -= blockSize;
+
+    //     if (outputByteLen > 0)
+    //         KeccakF1600(state);
+    // }
 
     
 }
 
 int main() {
-
-    
-    KeccakF1600(&sample_input);
+    Keccak(256, 1344, sample_input_2, 1024, ',', "TODO", 512);
+    //KeccakF1600(&sample_input);
 }
